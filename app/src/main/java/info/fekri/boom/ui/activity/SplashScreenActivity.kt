100dd6d6d5b97c8014db5045583009ef9f3a59bc
fekri86114 <fekri.mohammadreza86@gmail.com>
2023-03-22 11:19:04 -0700

    splash screen is done | added neumorphism style

--- app/src/main/java/info/fekri/boom/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/info/fekri/boom/ui/activity/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package info.fekri.boom.ui.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -7,6 +8,7 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import info.fekri.boom.databinding.ActivitySplshScreenBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplshScreenBinding
 
@@ -16,10 +18,10 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            /* goes to MainActivity */
+            /* go to MainActivity */
             startActivity(Intent(this, MainActivity::class.java))
             finish()
-        }, 1000)
+        }, 3000)
 
     }
 }
